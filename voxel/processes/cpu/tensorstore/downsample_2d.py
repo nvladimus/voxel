8bf2498891f5d3f5c859b2ad7c8004883be8c46f
adamkglaser <adamkglaser@gmail.com>
2024-03-18 23:02:30 -0700

    adding tensorstore downsampling

---
+++ voxel/processes/cpu/tensorstore/downsample_2d.py
@@ -0,0 +1,14 @@
+import numpy
+import tensorstore as ts
+
+class DownSample2D:
+
+    def __init__(self, binning: int = 2):
+        # downscaling factor
+        self._binning = binning
+
+    def run(self, image: numpy.array):
+        downsampled_image = ts.downsample(ts.array(image),
+                                          downsample_factors=[self._binning, self._binning],
+                                          method="mean").read().result()
+        return downsampled_image
