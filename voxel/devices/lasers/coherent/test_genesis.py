5d37d6ae7cf3e9ea7f4b3eea874591482e7c2afc
Walter Mwaniki <walter.g.mwaniki@gmail.com>
2024-06-14 18:06:24 -0700

    add second laser to test

--- voxel/devices/lasers/coherent/test_genesis.py
+++ voxel/devices/lasers/coherent/test_genesis.py
@@ -1,12 +1,19 @@
 from voxel.devices.lasers import GenesisLaser
 
-TEST_LASER = {
+LASER1 = {
     "id": "test_laser",
     "conn": "A700467EP203"
 }
 
+LASER2 = {
+    "id": "test_laser",
+    "conn": "J687424BP914"
+}
+
 if __name__ == "__main__":
-    laser = GenesisLaser(TEST_LASER["id"], TEST_LASER["conn"])
-    print(laser.power_mw)
-    print(laser.power_setpoint_mw)
-    laser.close()
+    laser1 = GenesisLaser(LASER1["id"], LASER1["conn"])
+    laser2 = GenesisLaser(LASER2["id"], LASER2["conn"])
+    for laser in [laser1, laser2]:
+        print(laser.power_mw)
+        print(laser.power_setpoint_mw)
+        laser.close()
