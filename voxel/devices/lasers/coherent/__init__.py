b8955fcff944e39fe730871a7317e95ba1d4b9f8
waltermwaniki <walter.g.mwaniki@gmail.com>
2024-06-26 02:49:33 -0700

    refactor: update coherent obis lasers to implement revised base laser

--- voxel/devices/lasers/coherent/__init__.py
+++ voxel/devices/lasers/coherent/__init__.py
@@ -1,5 +1,14 @@
+"""
+This module provides voxel compatible classes for Coherent lasers.
+Coherent: https://www.coherent.com/
+"""
+
 from .genesis_mx import GenesisMXLaser
+from .obis_lx import ObisLXLaser
+from .obis_ls import ObisLSLaser
 
 __all__ = [
-    'GenesisMXLaser'
+    'GenesisMXLaser',
+    'ObisLXLaser',
+    'ObisLSLaser'
 ]
