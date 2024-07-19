6708c74475121aa708d8b82f06d7dc5f62a17ac8
Micah Woodard <micah.woodard@alleninstitute.org>
2024-07-19 10:17:00 -0700

    feat: adding functionality to update properties

--- voxel/descriptors/updating_property.py
+++
@@ -1,9 +0,0 @@
-
-class UpdatingProperty(property):
-    """Class that signals a property that needs to be updated continously"""
-
-    def __init__(self,  fget, fset=None, fdel=None, currently_updating=False):
-
-        super().__init__(fget=fget, fset=fset, fdel=fdel)
-
-        self.currently_updating = currently_updating
