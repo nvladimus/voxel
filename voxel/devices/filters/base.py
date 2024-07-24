3c12853d02e034ab89dd68dedd95f104bf6cb410
waltermwaniki <walter.g.mwaniki@gmail.com>
2024-07-23 19:17:58 -0700

    refactor: first attempt at making filter class the entry point for using Voxel filter devices

---
+++ voxel/devices/filters/base.py
@@ -0,0 +1,56 @@
+from abc import abstractmethod
+from typing import Optional
+
+from voxel.devices import VoxelDevice
+
+
+class VoxelFilterError(Exception):
+    """Custom """
+    pass
+
+
+class BaseFilter(VoxelDevice):
+    """Base class for all voxel compliant filters
+    Filters are typically part of a filter wheel. The wheel is responsible for moving the filter into the beam path.  \n
+    The filter wheel tracks the current filter in use. It will not allow two filters to be enabled at the same time.
+    The user must disable the current filter before enabling a new one.  \n
+    # TODO: Determine the best behavior for enabling a filter while another is enabled. Simplest way is to just disable
+        the current filter.
+    """
+
+    @abstractmethod
+    def enable(self) -> None:
+        """Enable the filter
+        """
+        pass
+
+    @abstractmethod
+    def disable(self) -> bool:
+        """Disable the filter
+        """
+        pass
+
+    @abstractmethod
+    def is_active(self) -> bool:
+        """Check if the filter is currently enabled
+        """
+        pass
+
+
+class BaseFilterWheel(VoxelDevice):
+
+    @abstractmethod
+    def add_filter(self, name: str, position: int):
+        """Add a filter to the wheel."""
+        pass
+
+    @abstractmethod
+    def set_filter(self, filter_name: str) -> None:
+        """Set the filterwheel to the specified filter."""
+        pass
+
+    @property
+    @abstractmethod
+    def current_filter(self) -> Optional[str]:
+        """Return the name of the currently active filter, or None if no filter is active."""
+        pass
