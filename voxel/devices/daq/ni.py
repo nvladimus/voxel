f8503379758cb3bb40273c3a3210f37ca2af6145
micahwoodard <micah.woodard@alleninstitute.org>
2024-07-26 08:34:21 -0700

    feat: fixing ni bug

--- voxel/devices/daq/ni.py
+++ voxel/devices/daq/ni.py
@@ -279,8 +279,8 @@ class DAQ(BaseDAQ):
                                              )
 
             # sanity check voltages for ni card range
-            max = getattr(self, 'ao_max_volts', 5)
-            min = getattr(self, 'ao_min_volts', 0)
+            max = getattr(self, 'max_ao_volts', 5)
+            min = getattr(self, 'min_ao_volts', 0)
             if numpy.max(voltages[:]) > max or numpy.min(voltages[:]) < min:
                 raise ValueError(f"voltages are out of ni card range [{max}, {min}] volts")
 
