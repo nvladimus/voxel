4eb5a6252179c236f09b0f346253921e7aca87ec
adamkglaser <adamkglaser@gmail.com>
2024-10-22 09:31:36 -0700

    updating and unit testing new rank downscale version

--- src/voxel/processes/downsample/gpu/gputools/rank_downsample_3d.py
+++ src/voxel/processes/downsample/gpu/gputools/rank_downsample_3d.py
@@ -1,10 +1,9 @@
-from numbers import Number
-from pathlib import Path
-
 import numpy
-from gputools import OCLArray, OCLProgram
 from mako.template import Template
-
+from numbers import Number
+from gputools import OCLArray, OCLProgram
+from gputools.core.ocltypes import cl_buffer_datatype_dict
+from gputools.convolve._abspath import abspath
 from voxel.processes.downsample.base import BaseDownSample
 
 
@@ -24,42 +23,32 @@ class GPUToolsDownSample3D(BaseDownSample):
     """
 
     def __init__(self, binning: int, rank=None, data_type=None):
+
         super().__init__(binning)
-        self._rank = None
+
+        self._binning = binning
+        if isinstance(self._binning, Number):
+            self._binning = (int(self._binning),) * 2
 
         self._rank = rank
-        # opencl kernel
-        with open(Path(__file__).absolute().parent/"rank_downscale_3d.cl", "r") as f:
-            self._kernel = Template(f.read())
-
-        # set binning
-        if isinstance(binning, Number):
-            binning = (int(binning),)*3
-
-        if len(binning) != 3:
-            raise ValueError("size has to be a tuple of 3 ints")
-
-        self._binning = tuple(map(int, binning))
-
-        # determine rank order
         if self._rank is None:
             self._rank = numpy.prod(self._binning) // 2
         else:
             self._rank = self._rank % numpy.prod(self._binning)
 
-        # determine gpu data type
-        if data_type == 'uint16':
-            gpu_dtype = 'ushort'
-        elif data_type == 'uint8':
-            gpu_dtype = 'uchar'
+        if data_type == "uint8":
+            DTYPE = cl_buffer_datatype_dict[numpy.uint8]
+        elif data_type == "uint16":
+            DTYPE = cl_buffer_datatype_dict[numpy.uint16]
         else:
-            raise ValueError(f"Data type {data_type} not supported")
+            raise ValueError("Invalid data type: {}".format(self._data_type))
 
-        rendered = self._kernel.render(DTYPE=gpu_dtype,
-                                       FSIZE_X=self._binning[2],
-                                       FSIZE_Y=self._binning[1],
-                                       FSIZE_Z=self._binning[0],
-                                       CVAL=0)
+        with open(abspath("kernels/rank_downscale.cl"), "r") as f:
+            tpl = Template(f.read())
+
+        rendered = tpl.render(
+            DTYPE=DTYPE, FSIZE_X=self._binning[2], FSIZE_Y=self._binning[1], FSIZE_Z=self._binning[0], CVAL=0
+        )  # constant value
 
         self._prog = OCLProgram(src_str=rendered)
 
@@ -74,15 +63,16 @@ class GPUToolsDownSample3D(BaseDownSample):
         """
 
         x_g = OCLArray.from_array(image)
-        y_g = OCLArray.empty(tuple(s0//s for s, s0 in zip(self._binning, x_g.shape)), x_g.dtype)
+        y_g = OCLArray.empty(tuple(s0 // s for s, s0 in zip(self._binning, x_g.shape)), x_g.dtype)
 
-        self._prog.run_kernel("rank_3", y_g.shape[::-1], None, x_g.data, y_g.data,
-                              numpy.int32(x_g.shape[2]), numpy.int32(x_g.shape[1]),
-                              numpy.int32(x_g.shape[0]), numpy.int32(self._rank))
+        self._prog.run_kernel(
+            "rank_2",
+            y_g.shape[::-1],
+            None,
+            x_g.data,
+            y_g.data,
+            numpy.int32(x_g.shape[1]),
+            numpy.int32(x_g.shape[0]),
+            numpy.int32(self._rank),
+        )
         return y_g.get()
-
-
-if __name__ == "__main__":
-    x = numpy.random.randint(3, 11, size=(2048, 2048, 1)).astype(numpy.uint16)
-    downsample = GPUToolsDownSample3D(binning=(2, 2, 1), rank=-2, data_type='uint16')
-    y = downsample.run(x)
